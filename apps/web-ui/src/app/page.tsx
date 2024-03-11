1ec3c2fd48958c5cd2b8f28f63cf2c9c62bbce30
kdalton0518 <kdalton0518@gmail.com>
2024-03-11 00:01:46 -0300

    feat: change home page

--- apps/web-ui/src/app/page.tsx
+++ apps/web-ui/src/app/page.tsx
@@ -1,22 +1,21 @@
 import Link from "next/link";
 
-import { getProductList } from "@/api/product";
-import ProductCard from "@/components/feature/product/product-card";
-import { ProductPagination } from "@/components/feature/product/product-pagination";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const data = await getProductList({ page: 1, items: 9 });
-
   return (
-    <main className="dark:bg-zinc-950 p-4 flex flex-col justify-center items-center gap-6">
-      <div className="max-w-fit min-h-fit grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-2 gap-6">
-        {data.product_list.map((p) => (
-          <Link href={`/product/${p.id}`} key={p.id}>
-            <ProductCard {...p} />
-          </Link>
-        ))}
+    <main className="flex flex-col justify-center items-center">
+      <div className="px-10 py-28 flex flex-col gap-8 bg-gradient-to-r from-cyan-400 to-blue-400 dark:from-cyan-800 dark:to-blue-800 w-full">
+        <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+          Shop the Latest Trends
+        </h1>
+        <p className="max-w-[400px] text-zinc-800 dark:text-zinc-300 md:text-xl">
+          Discover our hand-picked selection of high-quality products.
+        </p>
+        <Link href="/product">
+          <Button>View products</Button>
+        </Link>
       </div>
-      <ProductPagination meta={data.meta} />
     </main>
   );
 }
